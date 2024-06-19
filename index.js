cf1987ea463f31a59dd2c1dd3bc428a6af42bf3e
Adonay Santos <adonay.rssantos@gmail.com>
2024-06-19 15:48:49 -0300

    Endpoit '/oi' adicionado & criação do script 'start' no packege.json

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+// Endpoit /oi -> Olá, Mundo!
+
+app.get('/oi', function(req, res) {
+  res.send('Olá, Mundo!')
+})
+
+app.listen(3000, function() {
+  console.log("Aplicação rodando em http://localhost:3000")
+})
